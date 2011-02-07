a899af9670f437d803aaba78cab517bd3efdc2e0
Dealga McArdle <tetha.z@gmail.com>
2011-02-07 12:45:54 +0000

    adjusted the script API to 34647.

--- mesh_edge_intersection_tools.py
+++ mesh_edge_intersection_tools.py
@@ -23,7 +23,7 @@ bl_info = {
     "author": "zeffii",
     "version": (0,4,3),
     "blender": (2, 5, 6),
-    "api": 34360,
+    "api": 34647,
     "category": "Mesh",
     "location": "View3D > EditMode > (w) Specials",
     "warning": "Still under development, bug reports appreciated",
