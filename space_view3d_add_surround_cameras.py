9a888215789b45f9b8935e3f082769fb8868c328
Brendon Murphy <meta.androcto1@gmail.com>
2012-06-24 03:25:37 +0000

    added tracker url

--- space_view3d_add_surround_cameras.py
+++ space_view3d_add_surround_cameras.py
@@ -22,6 +22,8 @@ bl_info = {
     'location': "View3D > Tool Shelf > Surround Projection panel",
     'description': "Setup cameras and create rendering scenes for n-screen surround projection.",
     'wiki_url': "http://wiki.blender.org/index.php/Extensions:2.6/Py/Scripts/3D_interaction/Surround_Projection_Tools",
+    'tracker_url': 'https://projects.blender.org/tracker/index.php?'\
+                   'func=detail&aid=29266',
     'version': (0,1,2),
     'blender': (2, 6, 0),
     'category': '3D View'
