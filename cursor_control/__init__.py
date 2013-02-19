104d11351f5b8bc3340f951a2a40de5002f27f8e
Sebastian Nell <codemanx@gmx.de>
2013-02-19 12:51:21 +0000

    Cursor control addon: Update for API change (draw handlers). Uses Panel.poll() to init overlays and doesn't remove draw handlers - wth? But no crash on quit like for other addons, see http://projects.blender.org/tracker/?func=detail&atid=498&aid=34312&group_id=9
    Removing mesh_vertex_slide.py from contrib, unmaintained copy in branches/broken_stuff/

--- cursor_control/__init__.py
+++ cursor_control/__init__.py
@@ -23,8 +23,8 @@
 bl_info = {
     "name": "Cursor Control",
     "author": "Morgan Mörtsell (Seminumerical)",
-    "version": (0, 7, 0),
-    "blender": (2, 59, 0),
+    "version": (0, 7, 1),
+    "blender": (2, 65, 4),
     "location": "View3D > Properties > Cursor",
     "description": "Control the Cursor",
     "warning": "buggy, may crash other addons", # used for warning icon and text in addons panel
@@ -46,11 +46,11 @@ if "local_var" in locals():
     imp.reload(history)
     imp.reload(memory)
 else:
-    from cursor_control import data
-    from cursor_control import ui
-    from cursor_control import operators
-    from cursor_control import history
-    from cursor_control import memory
+    from . import data
+    from . import ui
+    from . import operators
+    from . import history
+    from . import memory
 
 local_var = True
 
@@ -64,7 +64,9 @@ def register():
     bpy.types.VIEW3D_MT_snap.append(ui.menu_callback)
 
 def unregister():
-    # Register menu
+    history.VIEW3D_PT_cursor_history_init.handle_remove()
+    memory.VIEW3D_PT_cursor_memory_init.handle_remove()
+    # Unregister menu
     bpy.types.VIEW3D_MT_snap.remove(ui.menu_callback)
     bpy.utils.unregister_module(__name__)
 
