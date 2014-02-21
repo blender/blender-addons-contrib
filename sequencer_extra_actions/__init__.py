cc6fe6177a9224953c2848437b9dbb3e8650ca9a
CoDEmanX <codemanx@gmx.de>
2014-02-21 18:21:07 +0100

    Sequencer Extra Actions: Removed unused "api" in bl_info again, replaced tracker_url by most recent Phabricator task (patch).

--- sequencer_extra_actions/__init__.py
+++ sequencer_extra_actions/__init__.py
@@ -21,14 +21,13 @@ bl_info = {
     "author": "Turi Scandurra, Carlos Padial",
     "version": (3, 8),
     "blender": (2, 69, 0),
-    "api": 60991,
     "category": "Sequencer",
     "location": "Sequencer",
     "description": "Collection of extra operators to manipulate VSE strips",
-    "wiki_url": "http://wiki.blender.org/index.php/"
-    "Extensions:2.6/Py/Scripts/Sequencer/Extra_Sequencer_Actions",
-    "tracker_url": "http://projects.blender.org/tracker/index.php?func=detail"
-                   "&aid=30474",
+    "wiki_url": "http://wiki.blender.org/index.php/Extensions:2.6/Py/"
+        "Scripts/Sequencer/Extra_Sequencer_Actions",
+    "tracker_url": "https://developer.blender.org/T32532",
+    # old tracker_url: https://developer.blender.org/T30474
     "support": "COMMUNITY"}
 
 
