5721c7ffeab1bda31c22c4cb090d033e6ccb3f73
Brendon Murphy <meta.androcto1@gmail.com>
2014-07-21 20:36:00 +1000

    fix for toolshelf

--- space_view3d_add_surround_cameras.py
+++ space_view3d_add_surround_cameras.py
@@ -59,7 +59,9 @@ class AddSurroundCamerasPanel(bpy.types.Panel):
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'TOOLS'
     bl_label = "Surround Projection"
+    bl_category = 'Addons'
     bl_options = {'DEFAULT_CLOSED'}
+
     def draw(self, context):
         layout = self.layout
         col = layout.column(align=True)
