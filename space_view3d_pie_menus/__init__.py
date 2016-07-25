8ac8298217ef8aaa6662522c59d1d9dca5aa8b86
meta-androcto <meta.androcto1@gmail.com>
2016-07-25 20:41:30 +1000

    fix wiki url

--- space_view3d_pie_menus/__init__.py
+++ space_view3d_pie_menus/__init__.py
@@ -51,7 +51,7 @@ bl_info = {
     'description': 'Pie Menu Activate',
     'warning': '',
     "wiki_url": "http://wiki.blender.org/index.php/Extensions:2.6/Py/"
-        "Scripts3D_interaction/viewport_pies",
+        "Scripts/3D_interaction/viewport_pies",
     "tracker_url": "https://developer.blender.org/maniphest/task/edit/form/2/",
     'category': 'User Interface'
 }
