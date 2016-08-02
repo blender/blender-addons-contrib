0406f64a5b8c9239f67e8dbb02c943f737c48738
meta-androcto <meta.androcto1@gmail.com>
2016-08-02 18:30:43 +1000

    add materials content/presets re: T48990

--- materials_library_vx/sendmat.py
+++ materials_library_vx/sendmat.py
@@ -11,5 +11,5 @@ for cat in bpy.context.scene.matlib_categories:
         if "category" in mat.keys() and mat['category'] == cat.name:
             materials.append(mat.name)
     cats.append([cat.name, materials])
-with open("C:\\Users\\Dell\\Downloads\\blender-2.77-070416-win64\\blender-2.77.0-git.b27322e-AMD64\\2.77\\scripts\\addons_contrib\\materials_library_vx\\categories.txt", "w") as f: 
+with open("C:\\Users\\Dell\\Downloads\\blender-2.77-300716-win64\\blender-2.77.0-git.3d7cf80-AMD64\\2.77\\scripts\\addons_contrib\\materials_library_vx\\categories.txt", "w") as f: 
     f.write(json.dumps(cats, sort_keys=True, indent=4))
