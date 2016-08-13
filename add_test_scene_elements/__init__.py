4c8d9ae1c0772cfdf6f7f4c9fb1bcbcabdcf1379
meta-androcto <meta.androcto1@gmail.com>
2016-08-13 22:10:03 +1000

    test scenes, change category

--- add_test_scene_elements/__init__.py
+++ add_test_scene_elements/__init__.py
@@ -28,7 +28,7 @@ bl_info = {
     "wiki_url": "http://wiki.blender.org/index.php/Extensions:2.6"
     "/Py/Scripts",
     "tracker_url": "https://developer.blender.org/maniphest/task/edit/form/2/",
-    "category": "Tools"}
+    "category": "Object"}
 
 
 if "bpy" in locals():
