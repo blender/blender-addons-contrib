fc82a7e8d55d205ccae42636bb209d5868eeba6c
Dalai Felinto <dalai@blender.org>
2024-02-21 15:39:57 +0100

    Extensions: Unify the tooltip for the enable/disable extension
    I considered adding a poll_message here, but I will instead hope this gets implemented until
    we release Blender 4.2.

--- bl_pkg/bl_extension_ops.py
+++ bl_pkg/bl_extension_ops.py
@@ -1403,12 +1403,12 @@ class BlPkgPkgUninstall(Operator, _BlPkgCmdMixIn):
 
 
 class BlPkgPkgDisable_TODO(Operator):
-    """NOTE: this operation is not yet supported"""
+    """Turn off this extension"""
     bl_idname = "bl_pkg.extension_disable"
-    bl_label = "Disable the extension"
+    bl_label = "Disable extension"
 
     def execute(self, _context):
-        self.report({'WARNING'}, "Disabling not yet supported")
+        self.report({'WARNING'}, "Disabling themes is not yet supported")
         return {'CANCELLED'}
 
 
