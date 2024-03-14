8274c1b36231a7403a6d811736c63cfdc79d418e
Campbell Barton <ideasman42@gmail.com>
2024-03-14 12:38:06 +1100

    Extensions: fix error in tests with empty tags

--- bl_pkg/cli/blender_ext.py
+++ bl_pkg/cli/blender_ext.py
@@ -1965,7 +1965,7 @@ class subcmd_dummy:
                     fh.write("""id = "{:s}"\n""".format(pkg_idname))
                     fh.write("""name = "{:s}"\n""".format(pkg_name))
                     fh.write("""type = "add-on"\n""")
-                    fh.write("""tags = []\n""")
+                    fh.write("""tags = ["UV"]\n""")
                     fh.write("""maintainer = "Maintainer Name <username@addr.com>"\n""")
                     fh.write("""license = ["SPDX:GPL-2.0-or-later"]\n""")
                     fh.write("""version = "1.0.0"\n""")
